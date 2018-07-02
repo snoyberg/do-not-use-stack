83f099316d192f8f151316aeecab4ca488e96b8b
Tony Morris <tmorris@tmorris.net>
2018-07-02 19:50:04 +1000

    fuck stack

---
+++ Setup.hs
@@ -0,0 +1,20 @@
+import Control.Concurrent(threadDelay)
+import Control.Monad(when)
+import Data.Foldable(any)
+import Data.List(isInfixOf)
+import Distribution.Simple(defaultMain)
+import System.Environment(getEnvironment)
+import System.Exit(exitFailure)
+import System.IO(hPutStrLn, stderr)
+
+main ::
+  IO ()
+main =
+  let detectStackEnv env =
+        any ("snapshots" `isInfixOf`) (lookup "HASKELL_PACKAGE_SANDBOXES" env)
+  in  do  env <- getEnvironment
+          when (detectStackEnv env) $
+            do  hPutStrLn stderr "Do not use stack."
+                threadDelay 10000000
+                exitFailure
+          defaultMain
